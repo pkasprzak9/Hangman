0149d05c9643d37bea24b4e75b697ceec2d21643
pkasprzak9 <pawkasprzak9@gmail.com>
2023-05-16 14:23:37 +0200

    create method to check if letter is in word and to display guessed letters

--- lib/main.rb
+++ lib/main.rb
@@ -12,25 +12,35 @@ def correct_size(dictionary)
 end
 
 def choose_word(dictionary)
-  correct_size(dictionary).sample
+  correct_size(dictionary).sample.split('')
 end
 
-def guess
-  guesses_left = 9
-  until guesses_left.zero?
-    guess = ''
-    puts "#{guesses_left} guesses left"
-    guesses_left -= 1
-    puts 'Enter your guess: '
-    guess = gets.chomp.to_s until guess.length == 1
+def guess_letter(word, guess_array, used_letters, guesses_left)
+  puts "Already guessed letters:  #{used_letters.join('')}"
+  guess = ''
+  puts "#{guesses_left} guesses left"
+  puts 'Enter your guess: '
+  guess = gets.chomp.to_s until guess.length == 1
+  check_letter(word, guess, guess_array)
+  used_letters << "#{guess} "
+end
+
+def check_letter(word, guess, guess_array)
+  word.each_with_index do |letter, index|
+    guess_array[index] = " #{letter} " if letter == guess
   end
+  puts guess_array.join('')
 end
 
 def play_game
   file = CSV.open('dictionary.csv')
-  word = choose_word(file).split('')
+  word = choose_word(file)
   puts "Your word is: #{word.length} letters long"
-  guess
+  guesses_left = 9
+  guess_array = Array.new(word.length) { ' _ ' }
+  used_letters = []
+  puts guess_array.join('')
+  guess_letter(word, guess_array, used_letters, guesses_left) && guesses_left -= 1 until guesses_left.zero?
 end
 
 play_game
